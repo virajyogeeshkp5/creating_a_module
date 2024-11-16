c65f51e160251f052b795faa3c5861b26b51ea6e
YOGEESH K P <yogeesh793@gmail.com>
2024-11-16 20:54:31 +0530

    Add files via upload

---
+++ creating_a_module.py
@@ -0,0 +1,17 @@
+# creating a module
+
+def say_hello():
+    print("hello!")
+say_hello()
+
+# string function - str() and repr()
+
+s="w'o'w"
+print(repr(s))
+print(str(s))
+
+# date
+import datetime
+today= datetime.datetime.now()
+print(str(today))
+print(repr(today))
